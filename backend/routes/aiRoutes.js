671d412caa6af534fa901fe917dfa237aae61fdc
vansu@21 <vivekbisen21@gmail.com>
2026-07-08 13:32:38 +0530

    bug resolved

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const { categorize, chat } = require('../controllers/aiControllerGemini`');
+const { categorize, chat } = require('../controllers/aiControllerGemini');
 
 router.post('/categorize', auth, categorize);
 router.post('/chat', auth, chat);
